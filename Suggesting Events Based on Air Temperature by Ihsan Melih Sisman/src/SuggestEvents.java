fb231772843e63820478a2f52e0aa7bbd2dbb7a9
İhsan Melih Şişman <erensisman@windowslive.com>
2022-11-19 22:34:05 +0300

    commits were uploaded.

--- Suggesting Events Based on Air Temperature by Ihsan Melih Sisman/src/SuggestEvents.java
+++ Suggesting Events Based on Air Temperature by Ihsan Melih Sisman/src/SuggestEvents.java
@@ -1,4 +1,11 @@
 import java.util.Scanner;
+/*Conditions :
+
+Suggest to "Ski" if the temperature is less than 5.
+Suggest the "Cinema" event if the temperature is between 5 and 15.
+Suggest the "Picnic" event if the temperature is between 15 and 25.
+Suggest the "Swimming" event if the temperature is greater than 25.
+Find different solutions in how else the if conditions could be created on the same example.*/
 
 public class SuggestEvents {
     public static void main(String[] args) {
